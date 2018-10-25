b5e424ff4baa9676e35ac21f3b06b72b7705ed87
Fatin Adilah <Fatin Adilah@172.19.115.21>
2018-10-25 12:21:36 +0800

    update query for parent and child element

--- application/models/reference_list.php
+++ application/models/reference_list.php
@@ -7,14 +7,21 @@ class Reference_List_Model {
     }
 
     public function GetReferenceList($elementCode,$docNameId) {
-         $sql = "SELECT rma.element_code,rde.element_desc,rma.multi_answer_desc,rma.input_type, rma.doc_name_id, rma.sorting, rma.parent_element_code, rmi.ref_element_code "
-                    ."FROM document_element de "
-                    ."INNER JOIN ref_multiple_answer rma ON(de.parent_element_code=rma.element_code) "
-                    ."INNER JOIN ref_document_element rde ON(rde.element_code=de.parent_element_code) "
-                    ."LEFT JOIN ref_multiple_item rmi ON(rmi.element_code=rma.element_code AND rmi.multi_answer_desc=rma.multi_answer_desc) "
-                    ."WHERE rma.element_code = '" . (int) $elementCode . "' AND rma.doc_name_id = '$docNameId' "
-                    ."GROUP BY rma.multi_answer_desc "
-                    ."ORDER BY rde.element_desc,rma.sorting ";
+        $sql = "SELECT rma.element_code, rde.element_desc,rma.multiple_desc_code, rma.multi_answer_desc, rma.input_type, rma.sorting, rma.parent_element_code,rma.child_element_code, rma.show_label, rmi.ref_element_code "
+                . " FROM ref_multiple_answer rma "
+                . " LEFT JOIN ref_document_element rde ON(rde.element_code = rma.element_code) "
+                . " LEFT JOIN ref_multiple_item rmi ON(rmi.element_code=rma.element_code AND rmi.multi_answer_desc=rma.multi_answer_desc) "
+                . " WHERE rma.element_code='" . (int) $elementCode . "' AND rma.doc_name_id='$docNameId' "
+                . " GROUP BY rma.multi_answer_desc "
+                . " ORDER BY rde.element_desc,rma.sorting ";
+//         $sql = "SELECT rma.element_code,rde.element_desc,rma.multi_answer_desc,rma.input_type, rma.doc_name_id, rma.sorting, rma.parent_element_code, rmi.ref_element_code "
+//                    ."FROM document_element de "
+//                    ."INNER JOIN ref_multiple_answer rma ON(de.parent_element_code=rma.element_code) "
+//                    ."INNER JOIN ref_document_element rde ON(rde.element_code=de.parent_element_code) "
+//                    ."LEFT JOIN ref_multiple_item rmi ON(rmi.element_code=rma.element_code AND rmi.multi_answer_desc=rma.multi_answer_desc) "
+//                    ."WHERE rma.element_code = '" . (int) $elementCode . "' AND rma.doc_name_id = '$docNameId' "
+//                    ."GROUP BY rma.multi_answer_desc "
+//                    ."ORDER BY rde.element_desc,rma.sorting ";
         $this->db->connect();
         $this->db->prepare($sql);
         $this->db->queryexecute();
@@ -47,17 +54,11 @@ class Reference_List_Model {
     }
     
     public function GetChildMultipleAnswerList($elementCode,$docNameId){
-        $sql= "SELECT DISTINCT a.element_code,a.ref_element_code,a.doc_name_id,c.element_desc,b.multi_answer_desc,b.input_type,b.sorting,b.parent_element_code "  
+        $sql= "SELECT DISTINCT a.element_code,a.ref_element_code,a.doc_name_id,c.element_desc,a.show_label,b.multi_answer_desc,b.input_type,b.sorting,b.parent_element_code "  
                 ."FROM ref_multiple_item a "
                 ."LEFT JOIN ref_multiple_answer b ON(a.doc_name_id=b.doc_name_id AND a.element_code=b.element_code AND a.multi_answer_desc=b.multi_answer_desc) "
-                ."LEFT JOIN ref_document_element c ON(c.element_code=a.element_code) "
+                ."LEFT JOIN ref_document_element c ON(c.element_code=a.ref_element_code) "
                 ."WHERE a.element_code='" . (int) $elementCode . "' AND a.doc_name_id = '$docNameId' ";
-//        $sql="SELECT a.element_code,a.ref_element_code,b.multi_answer_desc,b.input_type "  
-//                ."FROM ref_multiple_item a "
-//                ."LEFT JOIN ref_multiple_answer b ON(a.doc_name_id=b.doc_name_id AND a.element_code=b.element_code AND a.multi_answer_desc=b.multi_answer_desc) "
-//                ."WHERE a.element_code='" . (int) $elementCode . "' AND a.doc_name_id='$docNameId' ";
-//        echo $sql;
-//        echo 'child';
         $this->db->connect();
         $this->db->prepare($sql);
         $this->db->queryexecute();
