8e10ce677eca89b21eb3a1aba26bde609ca5dded
Fatin Adilah <Fatin Adilah@172.19.115.21>
2018-10-31 17:49:44 +0800

    show create form tab, hide sql raw data

--- application/views/common/main.php
+++ application/views/common/main.php
@@ -36,10 +36,10 @@
                       <li><a href="<?php echo SITE_ROOT; ?>/formview/new-form">Add New Form</a></li>
                       <li><a href="<?php echo SITE_ROOT; ?>/formview/new-section">Add New Section</a></li>
                       <li><a href="<?php echo SITE_ROOT; ?>/formview/new-element">Add New Element</a></li>
-                      <li class="hidden"><a href="<?php echo SITE_ROOT; ?>/formview/form-builder">Form Builder</a></li>
+                      <li><a href="<?php echo SITE_ROOT; ?>/formview/form-builder">Form Builder</a></li>
                     </ul>
                     </li>
-                    <li><a href="<?php echo SITE_ROOT; ?>/formview/sql-raw-data">SQL Raw Data</a></li>
+                    <li class="hidden"><a href="<?php echo SITE_ROOT; ?>/formview/sql-raw-data">SQL Raw Data</a></li>
                     <li><a href='<?php echo SITE_ROOT; ?>/formview/generate-json-format'>Generate JSON Format</a></li>
                 </ul>
             </div>
