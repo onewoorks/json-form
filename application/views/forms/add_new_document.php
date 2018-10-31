6cb2db2850b308e6f4c54cddd11222bcd18c6ff2
Fatin Adilah <Fatin Adilah@172.19.115.21>
2018-10-31 17:42:05 +0800

    update popup for form creator

--- application/views/forms/add_new_document.php
+++ application/views/forms/add_new_document.php
@@ -1,61 +1,56 @@
-	<form id='addNewElement' class='form-horizontal'>
-    <div class='panel panel-default'>        
+<form id='editElement' class='form-horizontal'>
+    
+    <div class='panel panel-default'>
         <div class='panel-heading'>Properties</div>
         <div class='panel-body'>
-            <input type='hidden' name="section_number" value="<?= $section_no; ?>" />
-            
             <div class='form-group form-group-sm'>
-                <label class='control-label col-sm-4'>Element Description</label>
+                <label class='control-label col-sm-3'>Element Description</label>
                 <div class='col-sm-8'>
-                    <input type='text' name='element_desc' value='' class='form-control' autocomplete="off"/>
+                    <input type='text' name='element_desc' value='<?= $element; ?>' class='form-control' autocomplete="off"/>
                 </div>
             </div>
             
             <div class="form-group form-group-sm">
-                <label class="control-label col-sm-4">Position</label>
+                <label class="control-label col-sm-3">Position</label>
                 <div class="col-sm-8">
                       <label class="radio-inline">
-                          <input name="position" type="radio" value="L" checked="checked"> Left
+                            <input name="position" type="radio" value="L"> Left
                      </label>
                      <label class="radio-inline">
-                            <input name="position" type="radio" value="R" > Right
+                            <input name="position" type="radio" value="R"> Right
                      </label>
                 </div>
             </div>
             
             <div class='form-group form-group-sm'>
-                <label class='control-label col-sm-4'>Element Properties</label>
+                <label class='control-label col-sm-3'>Element Properties</label>
                 <div class='col-sm-8'>
                     <label class='radio-inline'>
-                        <input type='radio' name='element_properties' value='DECORATION' checked="checked" /> Decoration
+                        <input type='radio' name='element_properties' value='DECORATION_NEW'/> Decoration
                     </label>
                     <label class='radio-inline'>
-                        <input type='radio' name='element_properties' value='BASIC' /> Basic
-                    </label>
-                    <label class='radio-inline'>
-                        <input type='radio' name='element_properties' value='METHOD' /> Method
+                        <input type='radio' name='element_properties' value='BASIC_NEW' checked="checked"/> Basic
                     </label>
                 </div>
                 <div id='formelement'></div>
             </div>   
-            
         </div>
     </div>
 
     <div class='form-group form-group-sm'>
         <label class='control-label col-sm-3'></label>
         <div class='col-sm-12 text-right'>
-            <button type='submit' class='btn btn-sm btn-primary'>Save</button>
+            <button type='submit' class='btn btn-sm btn-primary'>Update</button>
         </div>
     </div>       
-</form>          
+</form>
+
 <script>
-    
-//DISPLAY ELEMENT PROPERTIES
+    //DISPLAY PROPERTY'S DETAIL   
     $(function () {
-        var $formType = 'decoration';
-        ElementBuilder($formType);
-        $('[name=form_element').val($formType);
+        var formType = $('input[name=element_properties]:checked').val();
+        ElementBuilder(formType);
+        $('[name=form_element').val(formType);
         $('[name=element_properties]').on('change', function () {
             var selector = $(this).val();
             $('#' + selector).show();
@@ -64,28 +59,18 @@
         });
     });
     
-//        function ElementBuilder($elementName) {
-//        $.ajax({
-//            url: '<?php echo SITE_ROOT;?>/formbuilder/formelement/',
-//            data: {value: $elementName},
-//            success: function (data) {
-//                $('#formelement').html(data);
-//            }
-//        });
-//    };
+    //BAWA KE PAGE->BASIC
+    function ElementBuilder(formType) {
+//        console.log(formType);
+        var formValue = $('#editElement').serializeArray();
 
-    
-//    $(function(){
-//        $('#addNewElement').submit(function(e){
-//            e.preventDefault();   
-//            alert('sampai');
-//            $.ajax({
-//                url : '<?= SITE_ROOT;?>/formview/pass-element/',
-//                data : { values: $(this).serializeArray(),basic: $('#basic').serializeArray(),rowinput:$('#rowinput').serializeArray()},
-//                success : function(){
-//                }
-//            });
-//        });
-//        });   
+        $.ajax({
+            url: '<?php echo SITE_ROOT;?>/formbuilder/formelement/',
+            data: {value:formType, params:formValue},
+            success: function (data) {
+                $('#formelement').html(data);
+            }
+        });
+    };
+            
 </script>
-
