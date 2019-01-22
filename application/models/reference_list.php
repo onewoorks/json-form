c9076b2f6efa09534234b9931f7d12b7aef5bcec
Fatin Adilah <Fatin Adilah@172.19.115.21>
2019-01-22 16:49:39 +0800

    re-edit query for parent & child

--- application/models/reference_list.php
+++ application/models/reference_list.php
@@ -10,7 +10,7 @@ class Reference_List_Model {
         $sql = "SELECT rma.element_code, rde.element_desc,rma.multiple_desc_code, rma.multi_answer_desc, rma.input_type, rma.sorting, rma.parent_element_code,rma.child_element_code, rma.show_label, rmi.ref_element_code "
                 . " FROM ref_multiple_answer rma "
                 . " LEFT JOIN ref_document_element rde ON(rde.element_code = rma.element_code) "
-                . " LEFT JOIN ref_multiple_item rmi ON(rmi.element_code=rma.element_code AND rmi.multi_answer_desc=rma.multi_answer_desc) "
+                . " LEFT JOIN ref_multiple_item rmi ON(rmi.element_code=rma.element_code AND rmi.multiple_desc_code=rma.multiple_desc_code AND rmi.doc_name_id=rma.doc_name_id) "
                 . " WHERE rma.element_code='" . (int) $elementCode . "' AND rma.doc_name_id='$docNameId' "
                 . " GROUP BY rma.multi_answer_desc "
                 . " ORDER BY rde.element_desc,rma.sorting ";
@@ -56,7 +56,7 @@ class Reference_List_Model {
     public function GetChildMultipleAnswerList($elementCode,$docNameId){
         $sql= "SELECT a.element_code,a.ref_element_code,a.doc_name_id,c.element_desc,a.show_label,b.multiple_desc_code,b.multi_answer_desc,b.input_type,b.sorting,b.parent_element_code "  
                 ."FROM ref_multiple_item a "
-                ."LEFT JOIN ref_multiple_answer b ON(a.doc_name_id=b.doc_name_id AND a.element_code=b.element_code AND a.multi_answer_desc=b.multi_answer_desc) "
+                ."LEFT JOIN ref_multiple_answer b ON(a.doc_name_id=b.doc_name_id AND a.element_code=b.element_code AND a.multiple_desc_code=b.multiple_desc_code) "
                 ."LEFT JOIN ref_document_element c ON(c.element_code=a.ref_element_code) "
                 ."WHERE a.element_code='" . (int) $elementCode . "' AND a.doc_name_id = '$docNameId' ";
         $this->db->connect();
