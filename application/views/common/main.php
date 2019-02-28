87e3d443cef197814d27b6c4dbb10f460ca90dca
fatinadilah <fatinadilah1207@gmail.com>
2019-02-28 15:26:37 +0800

    Update default page for different projects.

--- application/views/common/main.php
+++ application/views/common/main.php
@@ -2,12 +2,11 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>CD Document JSON Generator</title>
+        <title>CD JSON Generator</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="json form formatted from structured dataset">
         <meta name="author" content="irwan ibrahim">
         <link rel="shortcut icon" href="<?php echo SITE_ASSET; ?>/assets/favicon-JSON.ico" type="image/x-icon">
-        <link rel="icon" href="<?php echo SITE_ASSET; ?>/assets/favicon.ico" type="image/x-icon">
         <link href="<?php echo SITE_ASSET; ?>/assets/css/main.css" rel="stylesheet" />
         <link href="<?php echo SITE_ASSET; ?>/assets/library/bootstrap/css/bootstrap.css" rel="stylesheet">
         <script src="<?php echo SITE_ASSET; ?>/assets/library/ajax/jquery/2.1.4/jquery.js"></script>
@@ -26,36 +25,47 @@
         </style>
     </head>
 
-    <?php if(PROJECT_PATH === 'cd' || PROJECT_PATH === 'rispac'):?>
-    <body>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav">
-                    <li><a href='<?php echo SITE_ROOT; ?>/formview/generate-json-format'>Generate JSON Format</a></li>
-                </ul>
-            </div>
-        </nav>
-    </body>
-    <?php else:?>
-    <body>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav">
-                    <li><a href="<?php echo SITE_ROOT; ?>">List Of Documents</a></li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo SITE_ROOT; ?>/formview/new-form">Create New Form
-                            <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="<?php echo SITE_ROOT; ?>/formview/new-form">Add New Form</a></li>
-                            <li><a href="<?php echo SITE_ROOT; ?>/formview/new-section">Add New Section</a></li>
-                            <li><a href="<?php echo SITE_ROOT; ?>/formview/new-element">Add New Element</a></li>
-                            <li><a href="<?php echo SITE_ROOT; ?>/formview/form-builder">Form Builder</a></li>
-                        </ul>
-                    </li>
-                    <li class="hidden"><a href="<?php echo SITE_ROOT; ?>/formview/sql-raw-data">SQL Raw Data</a></li>
-                    <li><a href='<?php echo SITE_ROOT; ?>/formview/generate-json-format'>Generate JSON Format</a></li>
-                </ul>
-            </div>
-        </nav>
-    </body>
-    <?php endif;
+    <?php if (PROJECT_PATH === 'cd' || PROJECT_PATH === 'rispac'): ?>
+        <body>
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li><a href='<?php echo SITE_ROOT; ?>/formview/generate-json-format'>Generate JSON Format</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </body>
+    <?php elseif (PROJECT_PATH === 'uat'): ?>
+        <body>
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <!--<a class="navbar-brand" href="<?php echo SITE_ASSET; ?>/assets/favicon-JSON.ico"><img src="<?php echo SITE_ASSET; ?>/assets/favicon-JSON.ico" width="30" height="30" alt=""></a>-->
+                        <li><a href="<?php echo SITE_ROOT; ?>">List Of Documents</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo SITE_ROOT; ?>/formview/new-form">Create New Form
+                                <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo SITE_ROOT; ?>/formview/new-form">Add New Form</a></li>
+                                <li><a href="<?php echo SITE_ROOT; ?>/formview/new-section">Add New Section</a></li>
+                                <li><a href="<?php echo SITE_ROOT; ?>/formview/new-element">Add New Element</a></li>
+                                <li><a href="<?php echo SITE_ROOT; ?>/formview/form-builder">Form Builder</a></li>
+                            </ul>
+                        </li>
+                        <li class="hidden"><a href="<?php echo SITE_ROOT; ?>/formview/sql-raw-data">SQL Raw Data</a></li>
+                        <li><a href='<?php echo SITE_ROOT; ?>/formview/generate-json-format'>Generate JSON Format</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </body>
+    <?php else: ?>
+        <body>
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li><a href='<?php echo SITE_ROOT; ?>/formview/generate-json-format'>Generate JSON Format</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </body>
+        <?php endif;
