5d155e4249beb6e03164570a1d7c906f1bddcdb4
syazwanie <syazwanie@HTP9758.heitech.com.my>
2019-03-04 11:35:13 +0800

    update function edit

--- application/views/forms/new_section.php
+++ application/views/forms/new_section.php
@@ -1,62 +1,115 @@
 <?php echo $header; ?>
 
 <div id="sectionGroup">
-    <form id="sectionBuilder" class="form-horizontal">
-        <div class='panel-body'>
-            <div class='form-group form-group-sm'>
-                <label class='control-label col-sm-2'>Section Description&nbsp;<b style='color: red'>*</b></label>
-                <div class='col-sm-4'>
-                    <input type='text' name='section_desc' id='section_desc' class='form-control' autocomplete="off" required/>
-                </div>
-                <label class="control-label col-sm-2">Layout&nbsp;<b style='color: red'>*</b></label>
-                <div class="col-sm-4">
-                    <label class="radio-inline">
-                        <input name="layout" id="layout" type="radio" value="1" /> 1
-                    </label>
-                    <label class="radio-inline">
-                        <input name="layout" id="layout" type="radio" value="2" checked/> 2
-                    </label>
-                    <button type="button" class="btn btn-primary btn-sm pull-right addSection" disabled>Add Section</button>
 
+    <form id="sectionBuilder" class="form-horizontal col-md-offset-16 col-md-offset-1">
+        <div class='panel panel-default' style='margin:0px -20px 10px 0px'>
+            <div class="panel-heading">SECTION ADDITION</div>
+            <div class='panel-body'>
+                <div id='sectionGrouping'>   
+                    <div class="sectionNew1">
+                        <div class='form-group form-group-sm'>
+                            <!--<div class='row'>-->
+                            <label class='control-label col-sm-1'>Section Description&nbsp;<b style='color: red'>*</b></label>
+                            <div class='col-sm-3'>
+                                <input type='text' data-no = '1' name='section_desc1' id='section_desc1' class='form-control' autocomplete="off" required/>
+                                <!--20feb wani-->
+                                <span id='validateF1' name='validateF1' style="font-size:10px;color:red;text-align:left" hidden>Record Found</span>
+                                <span id='validateT1' name='validateT1' style="font-size:10px;color:green;text-align:left" hidden>No Record Found</span>
+                                <select id='list_section_desc' class='form-control hidden'>
+                                    <?php foreach ($list_of_sections as $sections): ?>
+                                        <option value='<?php echo $sections['section_code']; ?>'><?php echo $sections['section_desc']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+
+                            </div>
+
+                            <label class='control-label col-sm-1'>Json&nbsp;<b style='color: red'>*</b></label>
+                            <div class='col-sm-3'>
+                                <input type='text' name='json_desc1' data-no = '1' id='json_desc1' class='form-control' autocomplete="off" required disabled/>
+                                <span id='validateFF1' name='validateFF1' style="font-size:10px;color:red;text-align:left" hidden>Record Found</span>
+                                <span id='validateTT1' name='validateTT1' style="font-size:10px;color:green;text-align:left" hidden>No Record Found</span>
+                                <select id='list_json_desc' class='form-control hidden'>
+                                    <?php foreach ($list_of_sections as $sections): ?>
+                                        <option value='<?php echo $sections['section_desc']; ?>'><?php echo $sections['json_section']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+
+
+                            <label class="control-label col-sm-1">Layout&nbsp;<b style='color: red'>*</b></label>
+                            <div class="col-sm-2">
+                                <label class="radio-inline">
+                                    <input name="layout" id="layout" type="radio" value="1"> 1
+                                </label>
+                                <label class="radio-inline">
+                                    <input name="layout" id="layout" type="radio" value="2" /> 2
+                                </label>
+                            </div>
+                            <!--&nbsp;&nbsp;-->
+                            <!--<div class="col-sm-3">-->
+                            <div class='col-sm-1 sectionAction' data-sectionno='1'>
+                                <div class='btn btn-default btn-sm renameSection1' data-sectionno='1' style='padding:3.5px' title="Rename JSON"><i class='glyphicon glyphicon-pencil'></i></div>
+                                <div class='btn btn-default btn-sm plusSection' data-sectionno='1' style='padding:3.5px'><i class='glyphicon glyphicon-plus'></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 text-right">
+                    <button type="button" class="btn btn-primary btn-sm pull-right addSection" disabled>Add Section</button>
                 </div>
             </div>
         </div>
     </form>
+    <!--</div>-->
 
-    <div class='container-fluid col-md-12' style='margin-left: 40px;'>
-        <div class='panel panel-primary'>
-            <div class='panel-heading'>
-                <div class="btn-group pull-right">
+
+    <!--    <div class='container-fluid col-md-8' style='margin-left: 60px;'>
+            <div class='panel panel-primary'>
+                <div class='panel-heading'>
+                    <div class="btn-group pull-right">
+                    </div>-->
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class="col-md-offset-1 col-md-offset-1">
+                <div class='panel panel-primary'>
+                    <div class='panel-heading'>
+
+                        Result of Existing Section</div>
+                    <div class='panel-body'>
+                        <table id="tableForm" class='table table-bordered table-condensed'>
+                            <thead>
+                                <tr>
+                                    <th style=" font-size: smaller;">Section Code</th>
+                                    <th style=" font-size: smaller;">Section Desc</th>
+                                    <th style=" font-size: smaller;">JSON Section</th>
+                                    <th style=" font-size: smaller;">Layout</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if (!$list_of_sections): ?>
+                                    <tr>
+                                        <td colspan="7"><i>No Record Found</i></td>
+                                    </tr>
+                                <?php endif; ?>
+                                <?php
+                                $no = 1;
+                                foreach ($list_of_sections as $sections):
+                                    ?>
+                                    <tr>
+                                        <td  style=" font-size: smaller; text-align: center"><?php echo $sections['section_code']; ?></td>
+                                        <td  style=" font-size: smaller;"><?php echo $sections['section_desc']; ?></td>
+                                        <td  style=" font-size: smaller;"><?php echo $sections['json_section']; ?></td>
+                                        <td  style=" font-size: smaller;"><?php echo $sections['layout']; ?></td>
+                                    </tr>
+<?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-
-                Result of Existing Section</div>
-            <div class='panel-body'>
-                <table id="tableForm" class='table table-bordered table-condensed'>
-                    <thead>
-                        <tr>
-                            <th style=" font-size: smaller;">Section Code</th>
-                            <th style=" font-size: smaller;">Section Desc</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if (!$list_of_sections): ?>
-                            <tr>
-                                <td colspan="7"><i>No Record Found</i></td>
-                            </tr>
-                        <?php endif; ?>
-                        <?php $no = 1;
-                        foreach ($list_of_sections as $sections): ?>
-                            <tr>
-                                <td  style=" font-size: smaller; text-align: center"><?php echo $sections['section_code']; ?></td>
-                                <td  style=" font-size: smaller;"><?php echo $sections['section_desc']; ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
-
 </div>
 
 <script>
@@ -68,17 +121,26 @@
     $(function () {
 
         //ADDSECTION
+
         $('.addSection').click(function () {
-            var values = $('#sectionBuilder').serializeArray();
-            var layout = $("#layout").val();
-            var secDesc = $("#section_desc").val();
-            console.log(values);
+           $("input[id^='json_desc']").removeAttr('disabled');
+
+//            var values = $('#sectionBuilder').serializeArray();
+//            var layout = $("#layout").val();
+//            var secDesc = $("#section_desc").val();
+        
+
+//            console.log(values);
+
             $.ajax({
                 url: '<?= SITE_ROOT; ?>/formview/create-section/',
-                data: {values: values, layout: layout, secDesc: secDesc},
+                type: 'POST',
+//                data: {values: values, layout: layout, secDesc: secDesc},
+                data: {values: JSON.stringify($('#sectionBuilder').serializeArray())},
                 success: function (data) {
+                 console.log(data);
 //                alert(data);
-                    swal({
+                    swal({                        
                         title: "Section Created!",
                         text: "Data successfully inserted into database",
                         type: "success"
@@ -92,6 +154,7 @@
         });
     });
 </script>
+
 
 <script>
     $(document).ready(function () {
@@ -115,6 +178,173 @@
     });
 
 </script>
+
+<script>
+    $(document).ready(function () {
+        var no = 2;
+        var optionS = $("#list_section_desc").html();
+        var optionJ = $("#list_json_desc").html();
+
+
+        var selText;
+        var array = [];
+        var selText2;
+        var array2 = [];
+
+
+        $("#list_section_desc option").each(function () {
+            var $this = $(this);
+            selText = $this.text();
+            array.push(selText);
+        });
+
+        $("#list_json_desc option").each(function () {
+            var $this = $(this);
+            selText2 = $this.text();
+            array2.push(selText2);
+        });
+
+
+
+        $('#sectionGrouping').on('click', '.plusSection', function () {
+            var $sections = '<div class="sectionNew' + no + '">';
+            $sections += '<div class="form-group form-group-sm">';
+            $sections += '<label class="control-label col-sm-1">Section Description&nbsp;<b style="color: red">*</b></label>';
+            $sections += '<div class="col-sm-3">';
+            $sections += '<input type="text" data-no = "' + no + '" name="section_desc' + no + '" id="section_desc' + no + '" class="form-control" autocomplete="off" required/>';
+            $sections += '<span id="validateF' + no + '" name="validateF' + no + '" style="font-size:10px;color:red;text-align:left" hidden>Record Found</span>';
+            $sections += '<span id="validateT' + no + '" name="validateT' + no + '" style="font-size:10px;color:green;text-align:left" hidden>No Record Found</span>';
+            $sections += '<select id="list_section_desc" class="form-control hidden">' + optionS + '</select>';
+            $sections += '</div>';
+            $sections += '<label class="control-label col-sm-1">Json&nbsp;<b style="color: red">*</b></label>';
+            $sections += '<div class="col-sm-3">';
+            $sections += '<input type="text" data-no = "' + no + '" name="json_desc' + no + '" id="json_desc' + no + '" class="form-control" autocomplete="off" required disabled>';
+            $sections += '<span id="validateFF' + no + '" name="validateFF' + no + '" style="font-size:10px;color:red;text-align:left" hidden>Record Found</span>';
+            $sections += '<span id="validateTT' + no + '" name="validateTT' + no + '" style="font-size:10px;color:green;text-align:left" hidden>No Record Found</span>';
+            $sections += '<select id="list_json_desc" class="form-control hidden">' + optionJ + '</select>';
+            $sections += '</div>';
+            $sections += '<label class="control-label col-sm-1">Layout&nbsp;<b style="color: red">*</b></label>';
+            $sections += '<div class="col-sm-2">';
+            $sections += '<label class="radio-inline">';
+            $sections += '<input name="layout' + no + '" id="layout' + no + '"type="radio" value="1" > 1';
+            $sections += '</label>';
+            $sections += '<label class="radio-inline">';
+            $sections += '<input name="layout' + no + '" id="layout' + no + '" type="radio" value="2" > 2';
+            $sections += '</label>';
+//            $sections += '  &nbsp;&nbsp  ';
+            $sections += '</div>';
+            $sections += '<div class="col-sm-1 sectionAction" data-sectionno="' + no + '">';
+            $sections += '<div class="btn btn-default btn-sm renameSection' + no + '" data-sectionno="' + no + '" style="padding:3.5px" title="Rename JSON"><i class="glyphicon glyphicon-pencil"></i></div>&nbsp;';
+            $sections += '<div class="btn btn-default btn-sm minusSection' + no + '" data-sectionno="' + no + '" style="padding:3.5px"><i class="glyphicon glyphicon-minus"></i></div>';
+            $sections += '</div>';
+            $sections += '</div>';
+            $sections += '</div>';
+            $($sections).appendTo('#sectionGrouping');
+            no++;
+        });
+//        //MINUS SECTION
+//        $('#sectionGrouping').on('click', '.minusSection', function () {
+//            var dropid = $(this).data('sectionno');
+//            $('.sectionNew' + dropid).remove();
+//        });
+
+    });
+</script>
+
+
+<script>
+    $(document).ready(function () {
+
+        var selText;
+        var array = [];
+        var selText2;
+        var array2 = [];
+
+        var thisValue;
+
+        $("#list_section_desc option").each(function () {
+            var $this = $(this);
+            selText = $this.text();
+            array.push(selText);
+        });
+
+        $("#list_json_desc option").each(function () {
+            var $this = $(this);
+            selText2 = $this.text();
+            array2.push(selText2);
+        });
+
+        $(document).on('focus', 'input', function () {
+            thisValue = $(this).attr('data-no');
+
+            $('#section_desc' + thisValue).keyup(function () {
+                var str = $(this).val();
+
+                if (str !== "") {
+                    if (array.indexOf(str) > -1) {
+                        $('#validateT' + thisValue).attr('hidden', 'hidden');
+                        $('#validateF' + thisValue).attr('hidden', false);
+                    } else {
+                        $('#validateT' + thisValue).attr('hidden', false);
+                        $('#validateF' + thisValue).attr('hidden', 'hidden');
+                    }
+                } else {
+                    $('#validateT' + thisValue).attr('hidden', 'hidden');
+                    $('#validateF' + thisValue).attr('hidden', 'hidden');
+                }
+
+                var sections = $(this).val().toLowerCase().replace(/ /g, '_');
+                var json = sections.replace(/[^A-Z0-9]+/ig, '_');
+                $('#json_desc' + thisValue).val(json);
+
+                //JSONSECTION
+                if (json !== "") {
+                    if (array.indexOf(str) > -1) {
+                        $('#validateTT' + thisValue).attr('hidden', 'hidden');
+                        $('#validateFF' + thisValue).attr('hidden', false);
+                    } else {
+                        $('#validateTT' + thisValue).attr('hidden', false);
+                        $('#validateFF' + thisValue).attr('hidden', 'hidden');
+                    }
+                } else {
+                    $('#validateTT' + thisValue).attr('hidden', 'hidden');
+                    $('#validateFF' + thisValue).attr('hidden', 'hidden');
+                }
+            });
+
+            $(".renameSection" + thisValue).click(function () {
+                var renameid = $(this).data('sectionno');
+                console.log('renameid', renameid);
+                $('#json_desc' + renameid).removeAttr('disabled');
+                $('#json_desc' + renameid).keyup(function () {
+                    var str = $(this).val();
+
+                    if (str !== "") {
+                        if (array2.indexOf(str) > -1) {
+                            $('#validateTT' + renameid).attr('hidden', 'hidden');
+                            $('#validateFF' + renameid).attr('hidden', false);
+                        } else {
+                            $('#validateTT' + renameid).attr('hidden', false);
+                            $('#validateFF' + renameid).attr('hidden', 'hidden');
+                        }
+                    } else {
+                        $('#validateTT' + renameid).attr('hidden', 'hidden');
+                        $('#validateFF' + renameid).attr('hidden', 'hidden');
+                    }
+
+                });
+            });
+
+            $('#sectionGrouping').on('click', '.minusSection' + thisValue, function () {
+                var dropid = $(this).data('sectionno');
+                console.log('dropid', dropid);
+                $('.sectionNew' + dropid).remove();
+            });
+
+        });//endOfFocus
+
+    });//endOfDocument
+</script>   
 
 <?php
 echo $footer;
