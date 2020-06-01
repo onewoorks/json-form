9442cadf5c7dcd525ae0386f96a373d5e6b06901
zarithaa <zarithaa@HTP9765.heitech.com.my>
2020-06-01 10:09:49 +0800

    Add override element_group in add element

--- application/views/forms/add_element.php
+++ application/views/forms/add_element.php
@@ -2,81 +2,91 @@
 
     <div class='panel panel-default'>
         <br>    
-             <input type='hidden' name="doc_id" value="<?= $doc_id; ?>" />
-            <input type='hidden' name="section_code" value='<?= $section_id; ?>' />
-            <input type="hidden" name="section_sorting" value="<?= $section_sorting->section_sorting; ?>" />           
-            
-            <div class="form-group form-group-sm">
-                <label class='control-label col-sm-2'>Element Description</label>
-                <div class="col-sm-8">
-                    <select name="element_desc" class="form-control" >
-                        <?php foreach ($elements as $element): ?>
-                            <option value='<?php echo $element['element_code']; ?>'><?php echo $element['element_desc']; ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                <span style='color: red; position: inherit'>*</span>
-            </div>
+        <input type='hidden' name="doc_id" value="<?= $doc_id; ?>" />
+        <input type='hidden' name="section_code" value='<?= $section_id; ?>' />
+        <input type="hidden" name="section_sorting" value="<?= $section_sorting->section_sorting; ?>" />           
 
-            <div class="form-group form-group-sm">
-                <label class="control-label col-sm-2">Element Group</label>
-                <div class="col-sm-8">
-                    <select name="element_group" class="form-control" autocomplete="on">
-                        <?php foreach ($elements as $element): ?>
-                            <option value='<?php echo $element['element_code']; ?>'><?php echo $element['element_desc']; ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                
+        <div class="form-group form-group-sm">
+            <label class='control-label col-sm-2'>Element Description</label>
+            <div class="col-sm-8">
+                <select name="element_desc" id="element_desc" class="form-control" >
+                    <?php foreach ($elements as $element): ?>
+                        <option value='<?php echo $element['element_code']; ?>'><?php echo $element['element_desc']; ?></option>
+                    <?php endforeach; ?>
+                </select>
             </div>
-            
-            <div class='form-group form-group-sm'>
-                <label class='control-label col-sm-2'>Element Level</label>
-                <div class='col-sm-8 form-inline'>
-                    <input type='number' name='element_level' class='form-control' style="width:8%" autocomplete="off" required/>
-                <span style='color: red; position: inherit'>*</span>
-                </div>
-            </div>
-
-
-            <div class="form-group form-group-sm">
-                <label class="control-label col-sm-2">Position</label>
-                <div class="col-sm-8">
-                    <label class="radio-inline">
-                        <input name="position" type="radio" value="L" checked> Left
-                    </label>
-                    <label class="radio-inline">
-                        <input name="position" type="radio" value="R"> Right
-                    </label>
-                </div>
-            </div>
-
-            <div class='form-group form-group-sm'>
-                <label class='control-label col-sm-2'>Element Properties</label>
-                <div class='col-sm-8'>
-                    <label class='radio-inline'>
-                        <input type='radio' name='element_properties' value='DECORATION_NEW'/> Decoration
-                    </label>
-                    <label class='radio-inline'>
-                        <input type='radio' name='element_properties' value='BASIC' checked="checked"/> Basic
-                    </label>
-                    <label class='radio-inline'>
-                        <input type='radio' name='element_properties' value='SUBSECTION_NEW'/> Subsection
-                    </label>
-                </div>
-                <div id='formelement'></div>
-            </div>    
-            
-            <div class='form-group form-group-sm'>
-        <label class='control-label col-sm-3'></label>
-        <div class='col-sm-12 text-right' style="margin-left: -80px">
-            <button type='submit' class='btn btn-sm btn-primary'>Update</button>
+            <span style='color: red; position: inherit'>*</span>
         </div>
-    </div>      
-       
+
+        <div class="form-group form-group-sm">
+            <label class="control-label col-sm-2">Element Group</label>
+            <div class="col-sm-8">
+                <select name="element_group" id="element_group" class="form-control" >
+                    <?php foreach ($elements as $element): ?>
+                        <option value='<?php echo $element['element_code']; ?>'><?php echo $element['element_desc']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+
+        </div>
+
+        <div class='form-group form-group-sm'>
+            <label class='control-label col-sm-2'>Element Level</label>
+            <div class='col-sm-8 form-inline'>
+                <input type='number' name='element_level' class='form-control' style="width:8%" autocomplete="off" required/>
+                <span style='color: red; position: inherit'>*</span>
+            </div>
+        </div>
+
+
+        <div class="form-group form-group-sm">
+            <label class="control-label col-sm-2">Position</label>
+            <div class="col-sm-8">
+                <label class="radio-inline">
+                    <input name="position" type="radio" value="L" checked> Left
+                </label>
+                <label class="radio-inline">
+                    <input name="position" type="radio" value="R"> Right
+                </label>
+            </div>
+        </div>
+
+        <div class='form-group form-group-sm'>
+            <label class='control-label col-sm-2'>Element Properties</label>
+            <div class='col-sm-8'>
+                <label class='radio-inline'>
+                    <input type='radio' name='element_properties' value='DECORATION_NEW'/> Decoration
+                </label>
+                <label class='radio-inline'>
+                    <input type='radio' name='element_properties' value='BASIC' checked="checked"/> Basic
+                </label>
+                <label class='radio-inline'>
+                    <input type='radio' name='element_properties' value='SUBSECTION_NEW'/> Subsection
+                </label>
+            </div>
+            <div id='formelement'></div>
+        </div>    
+
+        <div class='form-group form-group-sm'>
+            <label class='control-label col-sm-3'></label>
+            <div class='col-sm-12 text-right' style="margin-left: -80px">
+                <button type='submit' class='btn btn-sm btn-primary'>Update</button>
+            </div>
+        </div>      
+
     </div>
 </form>
+<script>
+    $(document).ready(function () {
+        $('#element_desc').on('change', function () {
+            $('#element_group').val($(this).val());
+        });
 
+// init
+        $('#element_desc').change();
+    });
+
+</script>
 <script>
     //DISPLAY PROPERTY'S DETAIL   
     $(function () {
@@ -85,7 +95,7 @@
         $('[name=form_element').val(formType);
         $('[name=element_properties]').on('change', function () {
             var selector = $(this).val();
-          //  console.log("selector",selector);
+            //  console.log("selector",selector);
             $('#' + selector).show();
             $('[name=form_element').val(selector);
             ElementBuilder(selector);
@@ -103,7 +113,8 @@
                 $('#formelement').html(data);
             }
         });
-    };
+    }
+    ;
 
     //UPDATE_ELEMENT
     $(function () {
@@ -118,14 +129,14 @@
             var method = JSON.stringify($('#basicMethod').serializeArray());
             var multAns = JSON.stringify($('#basicMultAns').serializeArray());
             var subSec = JSON.stringify($('#basicSubSec').serializeArray());
-           // console.log("datas", datas);
+            // console.log("datas", datas);
 
             $.ajax({
                 url: '<?= SITE_ROOT; ?>/formview/add-new-element/',
                 type: 'POST',
                 data: {dummy: null, values: datas, basicMethod: method, basicMultAns: multAns, basicSubSec: subSec},
                 success: function (data) {
-              //      console.log(data);
+                    //      console.log(data);
                     $('#myModal').modal('hide');
                     swal({
                         title: "New Element Updated!",
@@ -134,13 +145,13 @@
                     });
                 }
             });
-              setTimeout(
+            setTimeout(
                     function () {
                         window.location.reload(true);
                     }, 1200);
             return false;
         });
-     //    $('.genForm').attr('disabled', false);
+        //    $('.genForm').attr('disabled', false);
     });
 
 </script>
