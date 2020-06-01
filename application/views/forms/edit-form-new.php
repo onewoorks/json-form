42361dce2eae9b2a73cf0b5fb97c2763a7be572c
zarithaa <zarithaa@HTP9765.heitech.com.my>
2020-06-01 09:32:33 +0800

    Remove modal title

--- application/views/forms/edit-form-new.php
+++ application/views/forms/edit-form-new.php
@@ -166,7 +166,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Add New Element </h4>
+                <h4 class="modal-title"> </h4>
             </div>
             <div class="modal-body">
             </div>
